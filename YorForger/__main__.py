7447c0c8068a1886b0fa92a06114016c7ad08f7e
Yash <yashprakash2005@gmail.com>
2022-05-31 00:47:45 +0530

    hmm

--- YorForger/__main__.py
+++ YorForger/__main__.py
@@ -719,7 +719,7 @@ def main():
 
     else:
         LOGGER.info(f"Yor_forger_spyxfamily_bot deployed. | BOT: [@Yor_forger_spyxfamily_bot]")
-        updater.start_polling(timeout=15, read_latency=4, clean=True)
+        updater.start_polling(timeout=15, read_latency=4, drop_pending_updates=True)
 
     if len(argv) not in (1, 3, 4):
         telethn.disconnect()
