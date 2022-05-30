f78728233da6e4de3c91e29fe38240c84541a6d0
Yash <yashprakash2005@gmail.com>
2022-05-31 01:18:53 +0530

    Update tagger.py

--- YorForger/modules/tagger.py
+++ YorForger/modules/tagger.py
@@ -40,16 +40,5 @@ async def _(event):
 
 __mod_name__ = "User Tagger"
 __help__ = """
-An "odds and ends" module for small, simple commands which don't really fit anywhere
-
-× /id: Get the current group id. If used by replying to a message, gets that user's id.
-× /info: Get information about a user.
-× /wiki : Search wikipedia articles.
-× /rmeme: Sends random meme scraped from reddit.
-× /ud <query> : Search stuffs in urban dictionary.
-× /wall <query> : Get random wallpapers directly from bot!
-× /reverse : Reverse searches image or stickers on google.
-× /paste: Saves replied content to `hastebin` and replies with a url.
-× /gdpr: Deletes your information from the bot's database. Private chats only.
-× /markdownhelp: Quick summary of how markdown works in telegram - can only be called in private chats.
+  × `/tagall` : Tag everyone in a chat
 """
