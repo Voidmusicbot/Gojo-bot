5dc1cb3b587b31c628f6aaf7f651f088c7835e2c
Yash <yashprakash2005@gmail.com>
2022-05-31 00:28:58 +0530

    Update __init__.py

--- YorForger/__init__.py
+++ YorForger/__init__.py
@@ -11,7 +11,7 @@ from pyrogram import Client, errors
 from redis import StrictRedis
 from telethon import TelegramClient
 from telethon.sessions import MemorySession
-from YorForger import config
+# from YorForger import config
 
 StartTime = time.time()
 
